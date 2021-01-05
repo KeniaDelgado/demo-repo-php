4530ef7e0c678f6402f993bef73d94f41bf21538
KeniaDelgado <kenia-5star@hotmail.com>
2021-01-05 16:47:34 -0700

    html body starts
    We set a defailt html body for the project

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
-<?php
- echo "<h3> Hola mundo </h3><br>";
- //Constantes
- define ("constante1","arreh");
- echo "Constante:".constante1;
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "<h3> Hola mundo </h3><br>";
+    //Constantes
+    define ("constante1","arreh");
+    echo "Constante:".constante1;
+    ?>
+</body>
+</html>
